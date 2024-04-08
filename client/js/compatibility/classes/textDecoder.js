3e9d1e32d62d32611bb4308cb77ec78ad63b83fa
xLuxy <67131061+xLuxy@users.noreply.github.com>
2024-04-08 02:12:19 +0200

    feat(compat, client): Add TextEncoder and TextDecoder

---
+++ client/js/compatibility/classes/textDecoder.js
@@ -0,0 +1,5 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
+
+cppBindings.registerCompatibilityExport("TextDecoder", alt.TextDecoder);
