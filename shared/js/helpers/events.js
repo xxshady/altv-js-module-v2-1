db646a2e58465613850c5689c93bee7842b2b44d
xshady <54737754+xxshady@users.noreply.github.com>
2024-06-12 20:33:42 +0300

    fix(server, compat): fix raw events API (#230)

--- shared/js/helpers/events.js
+++ shared/js/helpers/events.js
@@ -1,16 +1,25 @@
+/** @type {typeof import("../utils.js")} */
+const { assert } = requireBinding("shared/utils.js");
+
 /**
  * Creates a function that calls the emit function
  * with the rawEmitEnabled flag set to true.
  *
- * @param {string} fn
+ * @param {string | Function} fn
  * @returns {(eventName: string, ...args: unknown[]) => void}
  */
 export function emitRaw(fn = "emit") {
+    assert(fn != null);
+
     return function (eventName, ...args) {
         const oldValue = alt.Events.rawEmitEnabled;
 
         alt.Events.rawEmitEnabled = true;
-        this[fn](eventName, ...args);
+        if (typeof fn === "string") {
+            this[fn](eventName, ...args);
+        } else {
+            fn(eventName, ...args);
+        }
         alt.Events.rawEmitEnabled = oldValue;
     };
 }
